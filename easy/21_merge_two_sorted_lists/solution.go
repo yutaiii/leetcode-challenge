b43d4ad96e49ffae85f34e310c0def2a3aa564b9
市川裕太 <yu74ichi69@gmail.com>
2021-03-30 22:50:29 +0900

    [wip]20200330

--- easy/21_merge_two_sorted_lists/solution.go
+++ easy/21_merge_two_sorted_lists/solution.go
@@ -5,27 +5,19 @@
  *     Next *ListNode
  * }
  */
- func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
-    returnArr := make([]int, 0)
-    for _, v := range l1 {
-        returnArr = append(returnArr, v)
-    }
-    lenSum := len(l1) + len(l2) 
-    for _, v2 := range l2 {
-        for i, _ := range returnArr {
-            // 最後以外
-            if i + 1 < len(returnArr) {
-                if v2 > returnArr[i + 1] {
-                    
-                }
-            } else {
-                // 最後
-                
-            }
-        }
-    }
-    for i := 0; i < lenSum < i++ {
 
-    }
-    return returnArr
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	type ListNode struct {
+		Val  int
+		Next *ListNode
+	}
+
+	// わからん...
+	fmt.Printf("l1.Val %+v", l1.Val)
+	fmt.Println("=================")
+	fmt.Printf("l1.Next %+v", l1.Next)
+	fmt.Printf("l1.Val %+v", l1.Next.Val)
+	fmt.Println("=================")
+	fmt.Printf("l1.Next %+v", l1.Next.Next)
+	return l1
 }
