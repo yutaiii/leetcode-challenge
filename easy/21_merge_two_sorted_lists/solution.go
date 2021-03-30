bddf2f4a42d0d396bfc8c1456d186df2b0dbd7da
市川裕太 <yu74ichi69@gmail.com>
2021-03-31 00:17:45 +0900

    模範解答を追加

--- easy/21_merge_two_sorted_lists/solution.go
+++ easy/21_merge_two_sorted_lists/solution.go
@@ -5,19 +5,36 @@
  *     Next *ListNode
  * }
  */
-
+// hint: https://leetcode.com/problems/merge-two-sorted-lists/discuss/167437/go-solution
 func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
-	type ListNode struct {
-		Val  int
-		Next *ListNode
+	var newList = &ListNode{}
+	var out = newList
+	for l1 != nil && l2 != nil {
+		if l1.Val < l2.Val {
+			newList.Next = l1
+			// l1を1つ進める
+			l1 = l1.Next
+			newList = newList.Next
+			// fmt.Printf("if newList %+v", newList)
+			fmt.Printf("if out %+v", out)
+		} else {
+			newList.Next = l2
+			// l2を1つ進める
+			l2 = l2.Next
+			newList = newList.Next
+			fmt.Printf("else out %+v", out)
+		}
 	}
+	// 値はどこに保存されている？
 
-	// わからん...
-	fmt.Printf("l1.Val %+v", l1.Val)
-	fmt.Println("=================")
-	fmt.Printf("l1.Next %+v", l1.Next)
-	fmt.Printf("l1.Val %+v", l1.Next.Val)
-	fmt.Println("=================")
-	fmt.Printf("l1.Next %+v", l1.Next.Next)
-	return l1
+	fmt.Printf("newList %+v", newList)
+	fmt.Printf("out %+v", out)
+
+	// 最後に残ったものを格納
+	if l1 != nil {
+		newList.Next = l1
+	} else if l2 != nil {
+		newList.Next = l2
+	}
+	return out.Next
 }
